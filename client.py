6d44373124a78704eabe6122ef166e95285dfba3
Zayar Mg Htay <104290751+BorntoExplore@users.noreply.github.com>
2023-07-01 19:13:50 +0630

    Delete client.py

--- client.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-host=' 172.31.84.250'
-port = 8080
-
-s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), port))
-print(s.recv(1024).decode())
-s.close()
